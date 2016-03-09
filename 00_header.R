4e619ec1d196d306ad75a62e06645113040bcf4e
uhkniazi <uhkniazi@users.noreply.github.com>
2016-03-09 10:41:31 +0000

    get.prob function to calculate p.value from simulated data

--- 00_header.R
+++ 00_header.R
@@ -21,3 +21,19 @@ gcvChromosomes = c('Schisto_mansoni.Chr_ZW', 'Schisto_mansoni.Chr_1', 'Schisto_m
                    'Schisto_mansoni.Chr_4', 'Schisto_mansoni.Chr_3', 'Schisto_mansoni.Chr_6', 
                    'Schisto_mansoni.Chr_7', 'Schisto_mansoni.Chr_5')
 
+##### global functions
+## get p.value from simulated data
+get.prob = function(x, f){
+  # get the baseline
+  cBaseline = levels(f)[1]
+  ivBaseline = x[which(f == cBaseline)]
+  # remaining levels/groups to compare against
+  cvLevels = levels(f)[-1]
+  pret = sapply(cvLevels, function(l){
+    x.l = x[which(f == l)]
+    x.l.m = mean(x.l)
+    # calculate two sided p-value
+    return(min(c(sum(ivBaseline <= x.l.m)/length(ivBaseline), sum(ivBaseline >= x.l.m)/length(ivBaseline))) * 2)
+  })
+  return(pret)
+}
